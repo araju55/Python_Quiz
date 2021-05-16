0ce0fdb657a2692f018d4abf9cfdb08b5d7efff1
Amalamol Raju <33655795+araju55@users.noreply.github.com>
2021-05-16 18:11:47 +0100

    Add files via upload

---
+++ BibleQuiz.py
@@ -0,0 +1,81 @@
+#Quiz
+question1 = input("In the book of Genesis, Joseph is sold to which captain of the palace guard?")
+
+if question1 == "Potiphar" or question1 == "potiphar":
+    print("Correct")
+else:
+    print("Incorrect")
+    print("The correct answer is Potiphar")
+
+question2 = int(input("According to the book of Deuteronomy, how old was Moses when he died?"))
+
+if question2 == 120:
+    print("Correct")
+
+else:
+    print("Incorrect")
+    print("The correct answer is 120")
+
+question3 = input("To which tribe of Isreal did Samsom belong?")
+
+if question3 == "Tribe of Dan" or question3 == "tribe of dan":
+    print("Correct")
+else:
+    print("Incorrect")
+    print("The correct answer is Tribe of Dan")
+
+question4 = input("In Kings, what is the name of he priest that anoints Solomon with oils?")
+
+if question4 == "Zadok" or question4 == "zadok":
+    print("Correct")
+else:
+    print("Incorrect")
+    print("The correct answer is Zadok")
+
+question5 = input("According to Luke, the Ascension of Jesus took place in the vicinity of which village?")
+
+if question5 == "Bethany" or question5 == "bethany":
+    print("Correct")
+else:
+    print("Incorrect")
+    print("The correct answer is Bethany")
+
+question6 = input("In the Acts of the Apostles, which candidate was not chosen for the office of apostle after Judas Iscariot committed suicide?")
+
+if question6 == "Joseph Barsabbas" or "joseph barsabbas":
+    print("Correct")
+else:
+    print("Incorrect")
+    print("The correct answer is Joseph Barsabbas")
+
+question7 = input("What was the name of the blind beggar at the gate of Jericho?")
+
+if question7 == "Bartimaeus" or "bartimaeus":
+    print("Correct")
+else:
+    print("Incorrect")
+    print("The correct answer is Bartimaeus")
+
+question8 = input("In cubits, how long did God tell Noah to make the ark?")
+
+if question8 == "300 cubits" or "300 Cubits":
+    print("Correct")
+else:
+    print("Incorrect")
+    print("The correct answer is 300 cubits")
+
+question9 = input("Which queen, the wife of King Ahab, was thrown to her death from a window and trampled underfooot?")
+
+if question9 == "Queen Jezebel" or "queen jezebel":
+    print("Correct")
+else:
+    print("Incorrect")
+    print("The correct answer is Queen Jezebel")
+
+question10 = input("What did Jesus compare to mustard seed in a parable that appears in three of the Gospels?")
+
+if question10 == "Kingdom of God" or "kingdom of god":
+    print("Correct")
+else:
+    print("Incorrect")
+    print("The correct answer is Kingdom of God")
